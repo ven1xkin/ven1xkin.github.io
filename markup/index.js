b0a4a9b804542471294b13cb75c45a11bd280c85
ven1xkin <157031329+ven1xkin@users.noreply.github.com>
2026-01-28 00:42:53 +0800

    Add files via upload

--- markup/index.js
+++ markup/index.js
@@ -54,3 +54,36 @@ setInterval(() => {
 document.getElementById("surpriseBtn").onclick = () => {
   document.getElementById("surpriseText").classList.remove("hidden");
 };
+
+// 5️⃣ PHOTO CAROUSEL
+const images = [
+  "images/images(1).jfif",
+  "images/images(2).jfif",
+  "images/images(3).jfif",
+  "images/images(4).jfif",
+  "images/images(5).jfif",
+  "images/images(6).jfif",
+  "images/images(7).jfif",
+  "images/images(8).jfif",
+  "images/images(9).jfif"
+];
+
+let currentIndex = 0;
+const carouselImage = document.getElementById("carouselImage");
+
+function showImage(index) {
+  if (index < 0) currentIndex = images.length - 1;
+  else if (index >= images.length) currentIndex = 0;
+  else currentIndex = index;
+
+  carouselImage.src = images[currentIndex];
+}
+
+// Auto-slide every 3 seconds
+setInterval(() => {
+  showImage(currentIndex + 1);
+}, 3000);
+
+// Arrow buttons
+document.querySelector(".prev").onclick = () => showImage(currentIndex - 1);
+document.querySelector(".next").onclick = () => showImage(currentIndex + 1);
